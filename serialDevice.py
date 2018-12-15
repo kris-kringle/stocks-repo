203ec923526e623bd78a5c0572113f9270af6cae
Kris Kurtin <kriskurtin@gmail.com>
2018-12-15 14:29:46 -0500

    add exceptions to serialDevice.py

--- serialDevice.py
+++ serialDevice.py
@@ -3,26 +3,30 @@ import serial.tools.list_ports
 
 class serial_device:
 
-    def __init__(self, description, send_start = '', send_end = '\n', return_start = '', return_end = '\n', baudrate = 9600):
+    def __init__(self, description, send_start = '', send_end = '\n', return_start = '', return_end = '\n', baudrate = 9600, serialNumber = None):
 
         self.send_start_char = send_start
         self.send_end_char = send_end
         self.return_start_char = return_start
         self.return_end_char = return_end
 
-        self.serial_com_port = self.grabPort(description)
+        self.serial_com_port = self.grabPort(description, serialNumber)
         self.ser = serial.Serial(self.serial_com_port, baudrate = baudrate)
 
 
-    def grabPort(self, _description):
+    def grabPort(self, _description, serial_number):
 
         ports = list(serial.tools.list_ports.comports())
+        if len(ports) == 0:
+            raise Exception('No ports found')
+
         for p in ports:
             if p.description == _description:
-                my_port = p.device
-                return my_port
-            else:
-                my_port = 'No Port Found'
+                if serial_number is not None and p.serial_number == serial_number:
+                    my_port = p.device
+                    break
+            if p == ports[len(ports)]:
+                raise Exception('No port with given description found')
 
         return my_port
 
